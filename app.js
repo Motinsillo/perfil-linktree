b568edb4e8c926b8a4e4e65923a812742ec6eaa8
ALEJANDRO BARBOZA VELIZ <abarbozaveliz26@gmail.com>
2025-03-05 02:23:12 -0500

    se modifico fuente y ejercicio del DOM

---
+++ app.js
@@ -0,0 +1,12 @@
+//modificando h1
+const title = document.getElementById("titulo1")
+
+title.innerHTML= ("Sticky Wicks")
+
+// ingresando elemento h2
+const portador = document.getElementById("subtitle")
+portador.innerHTML =`
+<h2>Your local bajery Est. 1972</h2>
+`
+
+
